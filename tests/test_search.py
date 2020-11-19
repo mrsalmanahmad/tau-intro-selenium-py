641d02e9bb607950ec5691eccd373a6ed98a9027
mrsalmanahmad <salmanahmad3333@gmail.com>
2020-11-19 11:43:37 +0500

    added baseline test case

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,20 +1,24 @@
-
+from pages.result import duckduckgoResultPage
+from pages.search import duckduckgoSearchPage
 
 def test_basic_duckduckgo(browser):
-
+    search_page = duckduckgoSearchPage
+    result_page = duckduckgoResultPage
+    PHRASE = "panda"
     # Giver the duckduckgo homepage is displayed
     # TODO
-
+    search_page.load()
     # When the user searches for Panda
     # TODO
-
+    search_page.search(PHRASE)
     # Then the Search result should contain Panda
     # TODO
-
+    assert PHRASE in result_page.title()
     # And the search result query is Panda
     # TODO
-
+    assert PHRASE == result_page.search_input_value()
     # And the search result links pertain to panda
     # TODO
-
+    for title in result_page.result_link_titles():
+        assert PHRASE.lower in title.lower()
     raise Exception("Incomplete Test")
